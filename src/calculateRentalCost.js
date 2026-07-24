8fde20d845fc7701b98297a2779a8ccb4b8d2fd8
Daniil <dan201103@gmail.com>
2026-07-24 22:29:06 +0300

    first commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let sum = days * 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= 7) {
+    sum -= LONG_TERM_DISCOUNT;
+
+    return sum;
+  }
+
+  if (days >= 3) {
+    sum -= SHORT_TERM_DISCOUNT;
+
+    return sum;
+  }
+
+  return sum;
 }
 
 module.exports = calculateRentalCost;
